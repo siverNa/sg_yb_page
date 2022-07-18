799084a3dac9a817d0f9d62730542847cfdbaefc
siverNa <69504543+siverNa@users.noreply.github.com>
2022-07-18 21:28:44 +0900

    Feat: 추가 비밀번호 확인 코드
    - 입력된 비밀번호가 동일한 지 확인하는 코드 추가
    - 비밀번호가 동일하지 않은 경우, 에러메세지가 출력되도록 수정

--- sg_dir/php/signup_process.php
+++ sg_dir/php/signup_process.php
@@ -1,4 +1,14 @@
 <?php
+	function errPwMsg($msg) {
+		echo "
+			<script>
+				window.alert('$msg');
+				history.back(1);
+			</script>
+		";
+		exit;
+	}
+
 	require_once('./db_con.php');
 
 	switch($_GET['mode'])
@@ -7,16 +17,22 @@
 			$user_id = $_POST['user_id'];
 			$user_password = $_POST['user_password'];
 			$user_password_confirm = $_POST['user_password_confirm'];
-
-			$sql = "
+			
+			//비밀번호가 동일한지 확인
+			if ($user_password != $user_password_confirm)
+				errPwMsg("비밀번호가 일치하지 않습니다.");
+			else
+			{
+				$sql = "
 				INSERT INTO member (user_id, user_password)
 				VALUES ('$user_id', '$user_password');";
 			
-			$result = mysqli_query($connect, $sql);
-			if ($result === false)
-				echo mysqli_error($connect);
-			
-			header('Location: ../html/signup_ok.html');
+				$result = mysqli_query($connect, $sql);
+				if ($result === false)
+					echo mysqli_error($connect);
+				
+				header('Location: ../html/signup_ok.html');
+			}
 		break;
 	}
 
