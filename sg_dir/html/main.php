7d8478cd8f0a89e991c8f8191b8e479de7cdce5a
siverNa <69504543+siverNa@users.noreply.github.com>
2022-07-20 23:58:04 +0900

    Feat: 추가 로그인 페이지
    - 로그인 기능을 구현하기 위해, login.php 페이지 생성
    - session_start() 를 사용하기 위해, 기존 main.html 을 main.php 로 수정

---
+++ sg_dir/html/main.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<meta name = "viewport" content="width=device-width, initial-scale=1.0">
+		<title>main page</title>
+	</head>
+	<body>
+		<?php
+			if (!isset($_SESSION['user_id']))
+			{
+				echo '<p><a href="./signup.html">회원가입(signup)</a>';
+				echo '<a href="./login.html">로그인(signin)</a></p>';
+			}
+			else
+			{
+				echo '<div class="helloUser">'.$_SESSION['name'].'님 환영합니다.</div>';
+				echo '<div class="outAndUpdate"><a href="member/member_process.php?mode=logout">로그아웃 </a> | 
+				<a href="member/update.php">정보수정</a>
+				</div>';
+			}
+		?>
+		<p>이 페이지는 main page 입니다</p>
+	</body>
+</html>
