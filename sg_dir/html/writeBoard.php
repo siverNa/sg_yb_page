af77daff5c331eb5bc111f700aa82e2d5d5beeb0
siverNa <69504543+siverNa@users.noreply.github.com>
2022-07-26 21:31:50 +0900

    Feat: 추가 writeBoard.php, Board_process.php 코드
    - 게시판 작성을 위한 파일 2개 추가

---
+++ sg_dir/html/writeBoard.php
@@ -0,0 +1,37 @@
+<?php
+	require_once('../php/db_con.php');
+	session_start();
+	if (!$_SESSION['user_id'])
+		errPwMsg("로그인 후 작성할 수 있습니다.");
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>게시글 작성</title>
+</head>
+<body>
+	<p>
+	<?php
+		if (!isset($_SESSION['user_id']))
+		{
+			echo '<p><a href="./signup.html">회원가입(signup)</a>';
+			echo '<a href="./login.html">로그인(signin)</a></p>';
+		}
+		else
+		{
+			echo '<div class="helloUser">'.$_SESSION['user_id'].'님 환영합니다.</div>';
+			echo '<div class="outAndUpdate"><a href="../php/signup_process.php?mode=logout">로그아웃 </a> | 
+			<a href="member/update.php">정보수정</a>
+			</div>';
+		}
+	?>
+	</p>
+	<h2>글 작성</h2>
+	<form action="" method="post">
+		
+	</form>
+</body>
+</html>
