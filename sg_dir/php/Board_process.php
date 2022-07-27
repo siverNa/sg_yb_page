2ea3b7d6fcc3ba5fd48a61ef6f34b1f651fafd57
siverNa <69504543+siverNa@users.noreply.github.com>
2022-07-28 00:31:30 +0900

    Feat: 추가 board_process.php 기능
    - 파일명 수정
    - write를 전달받을 시, 데이터베이스에 게시글 내용을 저장하는 코드 생성

--- sg_dir/php/Board_process.php
+++ sg_dir/php/Board_process.php
@@ -1,3 +1,32 @@
 <?php
+	require_once('./db_con.php');
+	session_start();
 
+	switch ($_GET['mode'])
+	{
+		case 'write' :
+			if (!$_SESSION['user_id'])
+				errPwMsg("로그인을 먼저 해주십시오.");
+			else
+			{
+				$id = $_POST['id'];
+				$user_id = $_POST['user_id'];
+				$title = $_POST['title'];
+				$content = $_POST['content'];
+
+				$sql = "
+					INSERT INTO board(id, user_id, title, content, written, hit, liked)
+					VALUES ('$id', '$user_id', '$title', '$content', now(), 0, 0);
+				";
+				$result = mysqli_query($connect, $sql);
+				if ($result)
+				{
+					echo "<script>alert('게시글이 작성되었습니다');";
+					echo "window.location.replace('board.php');</script>";
+				}
+				else
+					echo mysqli_error($connect);
+			}
+		break;
+	}
 ?>
