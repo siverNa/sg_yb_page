ee75d46d1df257e4f654e37f7d5bc0ffbfea4677
siverNa <69504543+siverNa@users.noreply.github.com>
2022-07-28 23:18:22 +0900

    Feat:추가 BoardList.php 파일
    - 작성된 게시글 리스트를 보여주기 위한 페이지 생성
    - 동작이 정의되지않은 상태
    - table 태그만 추가된 상태

---
+++ sg_dir/html/BoardList.php
@@ -0,0 +1,47 @@
+<?php
+	require_once('../php/db_con.php');
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>게시글 목록</title>
+</head>
+<body>
+	<header>
+		<ul>
+			<a href="./main.php"><li>메인으로 돌아가기</li></a>
+			<a href="./Board_list.php"><li>게시판으로 이동</li></a>
+		</ul>
+		<?php
+			if (!isset($_SESSION['user_id']))
+			{
+				echo '<p><a href="./signup.html">회원가입(signup)</a>';
+				echo '<a href="./login.html">로그인(signin)</a></p>';
+			}
+			else
+			{
+				echo '<div class="helloUser">'.$_SESSION['user_id'].'님 환영합니다.</div>';
+				echo '<div class="outAndUpdate"><a href="../php/signup_process.php?mode=logout">로그아웃 </a> | 
+				<a href="member/update.php">정보수정</a>
+				</div>';
+			}
+		?>
+	</header>
+	<table>
+		<thead>
+			<tr>
+				<th width=70>번호</th>
+				<th width=300>제목</th>
+				<th width=120>작성자</th>
+				<th width=120>작성일</th>
+				<th width=70>조회수</th>
+				<th width=70>추천</th>
+			</tr>
+		</thead>
+	</table>
+</body>
+</html>
