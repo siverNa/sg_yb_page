e013d2f1355311f0cfcc2745978568e743b5f447
siverNa <69504543+siverNa@users.noreply.github.com>
2022-07-29 22:08:19 +0900

    Feat: 추가 viewBoard.php 파일
    - 게시글의 내용을 확인할 수 있는 view 페이지 생성
    - 페이지의 뼈대를 구성하는 코드 작성

---
+++ sg_dir/html/viewBoard.php
@@ -0,0 +1,42 @@
+<?php
+	require_once('../php/db_con.php');
+	session_start();
+
+	$num = $_GET['num'];
+	$sql = "
+		SELECT * FROM board WHERE num='$num'
+	";
+	$result = mysqli_query($connect, $sql);
+	$row = mysqli_fetch_array($result);
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>게시글 목록</title>
+</head>
+<body>
+	<header>
+		<ul>
+			<a href="./main.php"><li>메인으로 돌아가기</li></a>
+			<a href="./Board_list.php"><li>게시판으로 이동</li></a>
+		</ul>
+		<?php
+			if (!isset($_SESSION['user_id']))
+			{
+				echo '<p><a href="./signup.html">회원가입(signup)</a>';
+				echo '<a href="./login.html">로그인(signin)</a></p>';
+			}
+			else
+			{
+				echo '<div class="helloUser">'.$_SESSION['user_id'].'님 환영합니다.</div>';
+				echo '<div class="outAndUpdate"><a href="../php/signup_process.php?mode=logout">로그아웃 </a> | 
+				<a href="member/update.php">정보수정</a>
+				</div>';
+			}
+		?>
+	</header>
+</body>
+</html>
