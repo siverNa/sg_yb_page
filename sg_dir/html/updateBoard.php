3e2f90350345b3f6c01c66da14d8fdb0104d9f4b
siverNa <69504543+siverNa@users.noreply.github.com>
2022-08-02 19:51:09 +0900

    Feat: 추가 updateBoard.php 파일
    - 글 수정 기능을 추가하기 위한 파일 추가

---
+++ sg_dir/html/updateBoard.php
@@ -0,0 +1,15 @@
+<?php
+	
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>수정하기</title>
+</head>
+<body>
+	
+</body>
+</html>
