a529822889821e84f83c74241550090a90cfad00
siverNa <69504543+siverNa@users.noreply.github.com>
2022-08-11 23:53:43 +0900

    Feat: 추가 파일 업로드 기능
    - 파일 업로드 기능 추가
    - 이를 위해 writeBoard, Board_process 코드 수정
    - delete 알람에 삭제가 아닌 수정이라는 오탈자가 있어서 수정

--- sg_dir/php/Board_process.php
+++ sg_dir/php/Board_process.php
@@ -10,13 +10,37 @@
 			else
 			{
 				//$type = $_POST['type'];
+				//게시글 정보 변수들
 				$user_id = $_POST['user_id'];
 				$title = $_POST['title'];
 				$content = $_POST['content'];
+				//파일 정보 변수들
+				$error = $_FILES['file']['error'];
+				$tmp_file = $_FILES['file']['tmp_name'];
+				$file_name = $_FILES['file']['name'];
+				$file_size = $_FILES['file']['size'];
+				$file_type = $_FILES['file']['type'];
+				$dir = "../file/upload/".$file_name;
+
+				if ($error != UPLOAD_ERR_OK)
+				{
+					switch ($error)
+					{
+						case UPLOAD_ERR_INI_SIZE :
+							break;
+						case UPLOAD_ERR_FORM_SIZE :
+							echo "<script>alert('파일이 너무 큽니다.');";
+							echo "window.history.back()</script>";
+							exit;
+							break;
+					}
+				}
+				else
+					move_uploaded_file($tmp_file, $dir);
 
 				$sql = "
-					INSERT INTO board(user_id, title, content, written, hit, liked)
-					VALUES ('$user_id', '$title', '$content', now(), 0, 0);
+					INSERT INTO board(user_id, title, content, written, hit, liked, file)
+					VALUES ('$user_id', '$title', '$content', now(), 0, 0, '$file_name');
 				";
 				$result = mysqli_query($connect, $sql);
 				if ($result)
@@ -55,7 +79,7 @@
 			$result = mysqli_query($connect, $sql);
 			if ($result)
 			{
-				echo "<script>alert('게시글이 수정되었습니다');";
+				echo "<script>alert('게시글이 삭제되었습니다');";
 				echo "window.location.replace('../html/BoardList.php');</script>";
 			}
 			else
