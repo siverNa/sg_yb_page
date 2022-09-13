31b30f9b8906f5fc86cacff84ad0b94aa8a572d0
siverNa <69504543+siverNa@users.noreply.github.com>
2022-09-13 22:27:50 +0900

    Fix: 수정 댓글 작성 코드
    - 댓글 작성 코드 부분을 PDO 방식으로 수정

--- sg_dir/php/reply_ok.php
+++ sg_dir/php/reply_ok.php
@@ -6,11 +6,20 @@
 	$user_id = $_SESSION['user_id'];
 	if ($board_num && $user_id && $_POST['content'])
 	{
-		$sql = "
+		// $sql = "
+		// 	INSERT INTO reply(board_num, user_id, content, date)
+		// 	VALUES('$board_num', '$user_id', '".$_POST['content']."', now())
+		// ";
+		// $result = mysqli_query($connect, $sql);
+		$sql = $connect->prepare("
 			INSERT INTO reply(board_num, user_id, content, date)
-			VALUES('$board_num', '$user_id', '".$_POST['content']."', now())
-		";
-		$result = mysqli_query($connect, $sql);
+			VALUES(:board_num, :user_id, :content, now())
+		");
+		$sql->bindParam(':board_num', $board_num);
+		$sql->bindParam(':user_id', $user_id);
+		$sql->bindParam(':content', $_POST['content']);
+		$sql->execute();
+		$result = $sql->rowCount();
 		if ($result)
 		{
 			echo "<script>alert('댓글이 작성되었습니다.'); 
