59db4fa8534a76d3fcc1903364b889279a0f2450
siverNa <69504543+siverNa@users.noreply.github.com>
2022-09-13 22:04:30 +0900

    Fix: 수정 추천 및 추천 취소 코드
    - 추천 및 추천 취소에 대한 코드를 PDO 방식으로 수정

--- sg_dir/php/like_ok.php
+++ sg_dir/php/like_ok.php
@@ -5,10 +5,16 @@
 	$num = $_GET['num'];
 	$user_id = $_SESSION['user_id'];
 
-	$sql_check = "
-		SELECT * FROM board WHERE num=$num
-	";
-	$res_check = mysqli_fetch_array(mysqli_query($connect, $sql_check));
+	// $sql_check = "
+	// 	SELECT * FROM board WHERE num=$num
+	// ";
+	// $res_check = mysqli_fetch_array(mysqli_query($connect, $sql_check));
+	$sql_check = $connect->prepare("
+		SELECT * FROM board WHERE num=:num
+	");
+	$sql_check->bindParam(':num', $num);
+	$sql_check->execute();
+	$res_check = $sql_check->fetch();
 	if ($user_id == $res_check['user_id'])
 	{
 		echo "<script>alert('자신의 게시글입니다!');";
@@ -16,10 +22,17 @@
 		exit;
 	}
 
-	$sql_check2 = "
-		SELECT * FROM like_manager WHERE like_post_num='$num' and like_user='$user_id'
-	";
-	$res_check2 = mysqli_fetch_array(mysqli_query($connect, $sql_check2));
+	// $sql_check2 = "
+	// 	SELECT * FROM like_manager WHERE like_post_num='$num' and like_user='$user_id'
+	// ";
+	// $res_check2 = mysqli_fetch_array(mysqli_query($connect, $sql_check2));
+	$sql_check2 = $connect->prepare("
+		SELECT * FROM like_manager WHERE like_post_num=:num and like_user=:user_id
+	");
+	$sql_check2->bindParam(':num', $num);
+	$sql_check2->bindParam(':user_id', $user_id);
+	$sql_check2->execute();
+	$res_check2 = $sql_check2->fetch();
 	if ($res_check2)
 	{
 		echo "<script>alert('이미 추천한 게시글입니다!');";
@@ -27,11 +40,24 @@
 		exit;
 	}
 
-	$sql = "
-		UPDATE board SET liked=liked+1 WHERE num=$num;
-		INSERT INTO like_manager(like_post_num, like_user) VALUES ('$num', '$user_id');
-	";
-	$res = mysqli_multi_query($connect, $sql);
+	// $sql = "
+	// 	UPDATE board SET liked=liked+1 WHERE num=$num;
+	// 	INSERT INTO like_manager(like_post_num, like_user) VALUES ('$num', '$user_id');
+	// ";
+	// $res = mysqli_multi_query($connect, $sql);
+	$u_sql = $connect->prepare("
+		UPDATE board SET liked=liked+1 WHERE num=:num;
+	");
+	$u_sql->bindParam(':num', $num);
+	$u_sql->execute();
+
+	$i_sql = $connect->prepare("
+		INSERT INTO like_manager(like_post_num, like_user) VALUES (:num, :user_id);
+	");
+	$i_sql->bindParam(':num', $num);
+	$i_sql->bindParam(':user_id', $user_id);
+	$i_sql->execute();
+
 	echo "<script>alert('추천했습니다!');";
 	echo "window.history.back()</script>";
 ?>
--- sg_dir/php/unlike_ok.php
+++ sg_dir/php/unlike_ok.php
@@ -5,10 +5,17 @@
 	$num = $_GET['num'];
 	$user_id = $_SESSION['user_id'];
 
-	$sql_already = "
-		SELECT * FROM like_manager WHERE like_post_num='$num' and like_user='$user_id'
-	";
-	$res_already = mysqli_fetch_array(mysqli_query($connect, $sql_already));
+	// $sql_already = "
+	// 	SELECT * FROM like_manager WHERE like_post_num='$num' and like_user='$user_id'
+	// ";
+	// $res_already = mysqli_fetch_array(mysqli_query($connect, $sql_already));
+	$sql_already = $connect->prepare("
+		SELECT * FROM like_manager WHERE like_post_num=:num and like_user=:user_id
+	");
+	$sql_already->bindParam(':num', $num);
+	$sql_already->bindParam(':user_id', $user_id);
+	$sql_already->execute();
+	$res_already = $sql_already->fetch();
 	if (!$res_already)
 	{
 		echo "<script>alert('추천하지 않은 게시글입니다!');";
@@ -16,11 +23,24 @@
 		exit;
 	}
 
-	$sql = "
-		UPDATE board SET liked=liked-1 WHERE num=$num;
-		DELETE FROM like_manager WHERE like_post_num='$num' and like_user='$user_id';
-	";
-	$res = mysqli_multi_query($connect, $sql);
+	// $sql = "
+	// 	UPDATE board SET liked=liked-1 WHERE num=$num;
+	// 	DELETE FROM like_manager WHERE like_post_num='$num' and like_user='$user_id';
+	// ";
+	// $res = mysqli_multi_query($connect, $sql);
+	$u_sql = $connect->prepare("
+		UPDATE board SET liked=liked-1 WHERE num=:num;
+	");
+	$u_sql->bindParam(':num', $num);
+	$u_sql->execute();
+
+	$d_sql = $connect->prepare("
+		DELETE FROM like_manager WHERE like_post_num=:num and like_user=:user_id;
+	");
+	$d_sql->bindParam(':num', $num);
+	$d_sql->bindParam(':user_id', $user_id);
+	$d_sql->execute();
+
 	echo "<script>alert('추천을 취소했습니다!');";
 	echo "window.history.back()</script>";
 ?>
