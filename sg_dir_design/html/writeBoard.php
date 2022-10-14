e713af09011a805955e3afc52d442a161e9fbf1a
siverNa <69504543+siverNa@users.noreply.github.com>
2022-10-15 01:13:11 +0900

    Fix: 수정 이미지 업로드 방식
    - 이미지 업로드 방식 수정
    - 게시글 작성 시, 작성된 게시글로 리다이렉션 되도록 수정
    - Board_process.php 에서 마지막으로 추가된 게시글의 num을 반환하도록 수정

--- sg_dir_design/html/writeBoard.php
+++ sg_dir_design/html/writeBoard.php
@@ -15,9 +15,9 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	
 	<!-- include libraries(jQuery, bootstrap) -->
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
 	
 	<script src="../js/summernote/summernote-lite.js"></script>
 	<script src="../js/summernote/lang/summernote-ko-KR.js"></script>
@@ -33,7 +33,31 @@
 				maxHeight: null,             // 최대 높이
 				focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
 				lang: "ko-KR",					// 한글 설정
-				placeholder: '최대 2048자까지 쓸 수 있습니다'	//placeholder 설정	
+				placeholder: '최대 2048자까지 쓸 수 있습니다',	//placeholder 설정	
+				callbacks: {
+					// 파일 업로드시 동작하는 코드
+					// onImageUpload 이지만 비디오 드랍도 동작함.
+					onImageUpload: function(files) {
+						setFiles(files);
+					},
+					// 클립보드에 있는(윈도우 + 쉬프트 + s) 한 경우에 에디터에서 붙여넣기(컨트롤+v) 하는 경우
+					// 섬머노트 기본 이미지 붙여넣기 기능을 막는 코드.
+					// 없으면 이미지 2장씩 들어간다. ( 하나는 setFiles(file 형태) 로 하나는 base64(string 형태) 로 )
+					onPaste: function(e)
+					{
+						const clipboardData = e.originalEvent.clipboardData;
+						if (clipboardData && clipboardData.items && clipboardData.items.length) {
+							const item = clipboardData.items[0];
+							//indexOf(searchValue[, fromIndex]) 호출한 String 객체에서 주어진 값과 일치하는 첫 번째 인덱스를 반환.
+							//일치하는 값이 있으면 인덱스 리턴, 없으면 -1 리턴
+							// 붙여넣는게 파일이고, 이미지이면
+							if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
+								// 이벤트 막음
+								e.preventDefault();
+							}
+						}
+					}
+		   		},
 			});
 		});
 
@@ -44,9 +68,48 @@
 			// user_id, content를 가지고와서 formdata 로 전송
 			const user_id = document.getElementById('user_id').value;
 			const title = document.getElementById('title').value;
-			const content = $('#summernote').summernote('code');
+			let content = $('#summernote').summernote('code');
 
 			const formData = new FormData;
+
+			// 에디터 내부에 img, iframe 태그가 남아있는지 확인.
+			const summernoteWriteArea = document.getElementsByClassName("note-editable")[0];
+			const srcArray = [];
+			// getElementsByTagName 가 반환하는 형태는 HTMLCollection 인데 실제 배열이 없어서 forEach() 가 없음..
+			// 그래서 Array.from 로 array 로 만들어줌.
+			const iframeTags = Array.from(summernoteWriteArea.getElementsByTagName('iframe'));
+			const imgsTags = Array.from(summernoteWriteArea.getElementsByTagName('img'));
+
+			// 람다 사용함. ( 공부해보면 좋을 것.. )
+			iframeTags.forEach(iframe => {
+				srcArray.push(iframe.src);
+			});
+			imgsTags.forEach(img => {
+				srcArray.push(img.src);
+			});
+
+			const filesArrayLenght = filesArray.length;
+			for (let i = 0; i < filesArrayLenght; i++) {
+				const itrFile = filesArray[i];
+
+				// 에디터 안에 주소가 쓰이고 있으면
+				if (srcArray.includes(itrFile.name)) {
+
+					console.log(itrFile.name);
+
+					// 이유는 모르겠는데 서버에서 받는 파일 이름은 스키마나 baseUrl값이 없어져있었다.
+					// 그래서 여기서 문자열을 변환해주도록 만들었다.
+					const pathSplitArray = itrFile.name.split('/');
+					content = content.replace(itrFile.name, pathSplitArray[pathSplitArray.length - 1]);
+
+					// 왼쪽부터 (서버에서 받을때 사용할 파일 배열키, 파일)
+					// 서버에서 항상 배열로 받을려면 키 뒤에 '[]' 필요.
+					formData.append('files[]', itrFile);
+				}
+				// 이제 url 객체는 필요없으니까 메모리 해제
+				URL.revokeObjectURL(itrFile.name);
+			}
+
 			formData.append("user_id", user_id);
 			formData.append("title", title);
 			formData.append("content", content);
@@ -57,7 +120,7 @@
 				if (httpRequest.readyState === XMLHttpRequest.DONE) {
 					if (httpRequest.status === 200) {
 						console.log(httpRequest.response);
-						location.href = "./BoardList.php";
+						location.href = "./viewBoard.php?num=" + httpRequest.response;
 					} else {
 						alert("게시물 등록중 오류가 발생했습니다.");
 						button.disabled = false;
@@ -67,7 +130,58 @@
 			httpRequest.open('post', '../php/Board_process.php?mode=write', true);
 			httpRequest.send(formData);
 
+		}
+		
+		// filesArray 는 서버로 전송하기 전에 임시로 uri들을 들고 있는 배열이다.
+		const filesArray = [];
+
+		// 드래그앤 드랍시 동작하는 코드
+		function setFiles(files) {
+			const filesLenght = files.length;
+			for (let i = 0; i < filesLenght; i++) {
+				const file = files[i];
+
+				if (file.type.match('image.*')) {
+					// 임시 url 생성하는 부분
+					const url = URL.createObjectURL(file);
+					file.name = url;
+					// filesArray 이름을 방금 받은 url 로 담아둔다. (나중에 서버로 파일 보낼때 필요)
+					filesArray.push(new File([file], url, {
+						type: file.type
+					}));
+
+					// 에디터에 이미지 붙여넣기.
+					const imgElement = document.createElement("img");
+					imgElement.src = url;
+					const summernoteWriteArea = document.getElementsByClassName("note-editable")[0];
+					summernoteWriteArea.appendChild(imgElement);
+
+
+				} else if (file.type.match('video.*')) {
+					// 임시 url 생성하는 부분
+					const url = URL.createObjectURL(file);
+					console.log(file.type);
+					filesArray.push(new File([file], url, {
+						type: file.type
+					}));
+
+					const videoIframe = document.createElement("iframe");
+					videoIframe.src = url;
+					videoIframe.width = "640px";
+					videoIframe.height = "480px";
+					videoIframe.frameBorder = "0";
+					videoIframe.className = "note-video-clip";
+
+					// 에디터에 영상 붙여넣기 note-editable 에 붙여넣으면 됌.
+					const summernoteWriteArea = document.getElementsByClassName("note-editable")[0];
+					summernoteWriteArea.appendChild(videoIframe);
+
+					// 비디오나 이미지가 아니면
+				} else {
+					alert('지원하지 않는 파일 타입입니다.');
+				}
 			}
+		}
 	</script>
 	
 	<title>게시글 작성</title>
